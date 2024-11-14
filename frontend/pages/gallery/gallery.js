163a4f8284c2c38e06d9bd6279d4d0bea812cbd2
Sfvens <carlsensam1@gmail.com>
2024-11-14 12:30:06 -0700

    add gallery

--- frontend/pages/gallery/gallery.js
+++ frontend/pages/gallery/gallery.js
@@ -1,4 +1,12 @@
 const gallery = 
 `
-<p>gallery.html</p>
+<link rel="stylesheet" href="/pages/gallery/gallery.css">
+<div class="gallery-grid">
+  <img src="/assets/gallery/garden.jpg" class="gallery-image"></img>
+  <img src="/assets/gallery/government.jpg" class="gallery-image"></img>
+  <img src="/assets/gallery/puffy.jpg" class="gallery-image"></img>
+  <img src="/assets/gallery/teleportation.jpg" class="gallery-image"></img>
+  <img src="/assets/gallery/tortuga.jpg" class="gallery-image"></img>
+</div>
+<p class="attribution">Pictures from <a href="https://www.freepik.com">Freepik</a></p>
 `
